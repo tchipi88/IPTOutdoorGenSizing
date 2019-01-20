905d0a7c34b858d10804c08d5a8980b16e2743d7
tchipi88 <ngansop.arthur@gmail.com>
2019-01-20 23:28:04 +0100

    change formule

--- app/src/main/java/ihstowers/iptoutdoorgensizing/SplashcreenActivity.java
+++ app/src/main/java/ihstowers/iptoutdoorgensizing/SplashcreenActivity.java
@@ -3,7 +3,7 @@ package ihstowers.iptoutdoorgensizing;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 
 /**
  * An example full-screen activity that shows and hides the system UI (i.e.
